c7d6cbf39df046d7a6a63f7f5971fd9c96bcce60
Parker C Wilson <53058914+p4rkerw@users.noreply.github.com>
2020-02-10 08:50:09 -0600

    code for aggregating snRNAseq lib and agg.csv

---
+++ cellrangerRnaAggr.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# aggregate cellranger snrnaseq counts
+# TO RUN:
+# bash cellrangerRnaCount.sh aggregate_name path/to/aggregation.csv
+
+cellranger aggr \
+--id=cellranger_rna_aggr_control \
+--csv=crRnaAggr.csv \
+--normalize=none \
+--nosecondary
