d016a530fa266c885c4750a4bb19d07500aeb291
Parker C Wilson <53058914+p4rkerw@users.noreply.github.com>
2020-02-10 09:03:19 -0600

    update preprocessing for snATAC and snRNA

---
+++ snATAC_prep/cellrangerAtacCount.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# fastq files must follow naming convention eg.
+# libraryID_S1_L001_R1_001.fastq.gz
+# TO RUN:
+# bash cellrangerAtacCount.sh library_id path/to/fastq_dir
+# eg. bash cellrangerAtacCount.sh libraryID path/to/fastq
+
+# count the fastq files 
+cellranger-atac count \
+--id=$1 \
+--reference=/home/parkerw/reference/refdata-cellranger-atac-GRCh38-1.2.0 \
+--fastqs=$2 \
+--sample=$1
--- snRNA_prep/cellrangerRnaAggr.sh
+++ snRNA_prep/cellrangerRnaAggr.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # aggregate cellranger snrnaseq counts
 # TO RUN:
-# bash cellrangerRnaCount.sh aggregate_name path/to/aggregation.csv
+# bash cellrangerRnaCount.sh path/to/aggregation.csv
 
 cellranger aggr \
 --id=cellranger_rna_aggr_control \
